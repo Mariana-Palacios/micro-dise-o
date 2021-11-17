38d002ecaafe82858818f4e5652587348939b0c6
Mariana-Palacios <marianitaaax29@gmail.com>
2021-11-16 20:02:52 -0800

    graficas

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for
-from forms import LoginForm
+from forms import LoginForm, PostForm
 
 app = Flask(__name__)
 
@@ -9,12 +9,29 @@ app.config['SECRET_KEY']='25bc741c9b5d0fff1d69f1281da69b31'
 @app.route('/')
 def home():
     form = LoginForm()
-    return render_template('inicioSesion.html', title='Sign In', form=form)
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+    return render_template('inicioSesion.html', form=form)
 
 @app.route("/registro")
 def registro():
     form= FormularioRegistro()
     return render_template('registro.html',title='registro', form='form')
+
+@app.route("/admin/post/", methods=['GET', 'POST'], defaults={'post_id': None})
+@app.route("/admin/post/<int:post_id>/", methods=['GET', 'POST'])
+def post_form(post_id):
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        title_slug = form.title_slug.data
+        content = form.content.data
+        post = {'title': title, 'title_slug': title_slug, 'content': content}
+        posts.append(post)
+        return redirect(url_for('post_form'))
+    return render_template("graficas.html", form=form, title='registro')
 
 if __name__=="__main__":
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Email, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('Usuario', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    remember_me = BooleanField('Recuperar contaseña')
+    submit = SubmitField('Iniciar Sesion')
+
+class PostForm(FlaskForm):
+    title = StringField('Título', validators=[DataRequired(), Length(max=128)])
+    title_slug = StringField('Título slug', validators=[Length(max=128)])
+    content = TextAreaField('Contenido')
+    submit = SubmitField('Enviar')
