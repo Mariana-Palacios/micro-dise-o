c689e7b74adf450a28b7e9eaf761634c28a943b4
Mariana-Palacios <marianitaaax29@gmail.com>
2021-11-16 16:18:52 -0800

    introduccion form

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, request, url_for
-from formularios import FormularioRegistro, InicioSesionFormulario
+from forms import LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY']='25bc741c9b5d0fff1d69f1281da69b31'
 
-@app.route("/")
+#@app.route("/", methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    form= InicioSesionFormulario()
-    return render_template("inicioSesion.html", form='form')
+    form = LoginForm()
+    return render_template('inicioSesion.html', title='Sign In', form=form)
 
 @app.route("/registro")
 def registro():
-    formulario= FormularioRegistro()
+    form= FormularioRegistro()
     return render_template('registro.html',title='registro', form='form')
 
 if __name__=="__main__":
